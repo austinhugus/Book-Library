01e52c6672c7beda12491d72d800a5187f342016
austin <ap.hugus@gmail.com>
2020-07-07 13:51:29 -0600

    init

---
+++ .vscode/Models/Book.cs
@@ -0,0 +1,9 @@
+namespace console_library.Models {
+    public class Book {
+        //Props
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool Available { get; set; }
+
+    }
+}
